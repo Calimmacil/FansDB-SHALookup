bab2343e14096260cecce7bd2092fcff172efa4b
Calimmacil <vengefulmaestra@proton.me>
2024-12-09 13:36:46 -0500

    Removing extra functionality

--- plugins/SHALookup/SHALookup.py
+++ plugins/SHALookup/SHALookup.py
@@ -92,45 +92,13 @@ def add_sha256(sha256, oshash):
         return
     stash.file_set_fingerprints(scene["files"][0]["id"], {"type": "sha256", "value":sha256})
 
-def find_tags(search_tag):
-    log.debug(f"Looking for {search_tag}")
-    if stash.find_tag(search_tag):
-        # If the tag already exists, just return it.
-        log.debug("Tag already exists.")
-        return
-    # Need to check to see if we need to create an alias.
-    log.debug("Retrieving all tags.")
-    tags = stash.find_tags()
-    for tag in tags:
-        log.debug(f"Examining tag: '{tag}'")
-        no_space = tag["name"].replace(" ", "")
-        # Check if the tag name matches without spaces
-        if search_tag.lower() == no_space.lower():
-            log.debug(f"{search_tag} matches {tag}, adding alias.")
-            aliases = tag["aliases"]
-            log.debug(f"Adding {search_tag} to current aliases: {aliases}")
-            aliases.append(string.capwords(search_tag))
-            stash.update_tag({"id": tag["id"], "aliases": aliases})
-            return
-        # Check if any aliases match
-        for alias in tag["aliases"]:
-            ns = alias.replace(" ", "")
-            if search_tag.lower() == ns.lower():
-                aliases = tag["aliases"]
-                log.debug(f"Adding {search_tag} to current aliases: {aliases}")
-                aliases.append(string.capwords(search_tag))
-                stash.update_tag({"id": tag["id"], "aliases": aliases })
-                return
-    # No tags match. Give the user the option to create
-    return
-
 def getPostByHash(hash):
-    for attempt in range(1, 10):
+    for attempt in range(1, 5):
         shares = requests.get('https://coomer.su/api/v1/search_hash/' + hash, headers=headers, timeout=10)
         if shares.status_code == 200:
             break
         log.debug(f"Request status code: {shares.status_code}")
-        time.sleep(5)
+        time.sleep(2)
     shares.raise_for_status()
     data = shares.json()
     if (shares.status_code == 404 or len(data) == 0):
@@ -342,10 +310,6 @@ def scrape():
     if check_video_vertical(scene):
         result['Tags'].append({ 'Name': 'Vertical Video' })
     # if result, add tag
-    tags = re.findall(r'#(\w+)', result["Details"])
-    for tag in tags:
-        find_tags(tag)
-        result['Tags'].append({ "Name": tag })
     result['Tags'].append({ 'Name': success_tag })
     return result
 
